4593317a6125816d385f1e189e5a65005b9f01af
Benjamin Maxwell <benjamin.maxwell@arm.com>
2026-08-17 10:49:49 +0000

    Fixups

--- llvm/lib/Transforms/Vectorize/VPlan.h
+++ llvm/lib/Transforms/Vectorize/VPlan.h
@@ -2986,9 +2986,6 @@ public:
 
   void execute(VPTransformState &State) override;
 
-  InstructionCost computeCost(ElementCount VF,
-                              VPCostContext &Ctx) const override;
-
 #if !defined(NDEBUG) || defined(LLVM_ENABLE_DUMP)
   /// Print the recipe.
   void printRecipe(raw_ostream &O, const Twine &Indent,
